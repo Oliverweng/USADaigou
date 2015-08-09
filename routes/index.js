699498149674ce18011a636a6a8ac465f89233b9
Oliver Weng <oweng@asa.org>
2015-08-09 01:51:40 -0400

    added Yan LU

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+    res.render('helloworld', { title: 'Yan Lu' });
+});
+
 module.exports = router;
