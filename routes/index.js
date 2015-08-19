02f7ec9e54d64c2938abe63c8b08da4c8ee13fb4
Oliver Weng <oweng@asa.org>
2015-08-19 01:40:26 -0400

    api test work

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var http = require('https');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -57,17 +58,56 @@ router.post('/adduser', function(req, res) {
     });
 });
 
-/*
- * DELETE to deleteuser.
- */
-router.delete('/deleteuser/:id', function(req, res) {
-    var db = req.db;
-    var collection = db.get('userlist');
-    var userToDelete = req.params.id;
-    collection.remove({ '_id' : userToDelete }, function(err) {
-        res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
-    });
+router.post('/getScholarships', function(req, res) {
+	var stringJsonBody = JSON.stringify(req.body),
+        options = {
+            hostname: 'api.scholarshipexperts.com',
+            path: '/scholarshipfinder/v1/scholarships.json?auth=eec6029a-4c6d-4042-81d6-7e755c0cd21c',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+    sendXDomainRequest(options, function (data) {res.send('data is ' + data)}, 4000, function () {}, stringJsonBody);
 });
+
+function sendXDomainRequest(options, successCallBack, timeout, timeOutCallBack, reqBody) {
+    var req = http.request(options, function (response) {
+        var str = '';
+        response.on('data', function (chunk) {
+            str += chunk;
+            // console.log('got data with data chunk:' + chunk);
+        });
+        response.on('end', function () {
+            console.log('Cross Domain API Call Succeeded!');
+            var escaped = str.replace(/^throw [^;]*;/, '');
+            var jsonObj = JSON.parse(escaped);
+            successCallBack(jsonObj);
+        });
+    });
+    if (reqBody) {
+        req.write(reqBody);
+    }
+    req.end();
+    req.on('socket', function (socket) {
+        socket.setTimeout(parseInt(timeout, 10));
+        socket.on('timeout', function () {
+            console.log('Cross Domain API Call Timed Out!');
+            req.abort();
+            timeOutCallBack();
+        });
+    });
+    req.on('error', function (e) {
+        console.error(e);
+        successCallBack('');
+    });
+}
+ 
+// Search Scholarships
+exports.searchScholarships = function (jsonBody, successCallBack) {
+
+};
+
 
 
 module.exports = router;
