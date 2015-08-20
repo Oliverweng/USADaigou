5c33ee200af982132e1fa4ad369f6b698ab37511
Oliver Weng <oweng@asa.org>
2015-08-20 11:34:21 -0400

    adding package

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+// var cons = require('consolidate');
 // New Code
 var mongo = require('mongodb');
 var monk = require('monk');
@@ -18,6 +18,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+// app.engine('dust', cons.dust);
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'dust');
+// app.set('template_engine', 'dust');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
