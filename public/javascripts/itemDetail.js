687dc2e8c4e797132808bfbd198926a093f126de
Oliver Weng <Oliverweng@hotmail.com>
2016-01-02 00:29:31 -0500

    style update

--- public/javascripts/itemDetail.js
+++ public/javascripts/itemDetail.js
@@ -4,4 +4,22 @@ require(['jquery', 'underscore'], function ($, _) {
             src = $currentTarget.attr('src');
         $('#js-display-image').attr('src', src);
     });
+    $('.js-plus-btn').click(function (e) {
+        var $input = $('.js-quantity-input'),
+            val = $input.val();
+        if (val) {
+            $input.val(parseInt(val, 10) + 1);
+        } else {
+            $input.val('1');
+        }
+    });
+    $('.js-minus-btn').click(function (e) {
+        var $input = $('.js-quantity-input'),
+            val = $input.val();
+        if (val && val > 1) {
+            $input.val(parseInt(val, 10) - 1);
+        } else {
+            $input.val('1');
+        }
+    });
 });
